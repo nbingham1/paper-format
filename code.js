33269e370c424cc3ee589b07f22d5eb1650cfc48
Ned Bingham <edward.bingham@broccolimicro.io>
2024-12-31 18:31:30 -0500

    adding raw languages

--- code.js
+++ code.js
@@ -609,6 +609,52 @@ Prism.languages.chp = {
 	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
 };
 
+Prism.languages.rawchp = {
+	'probe': {
+		pattern: /#([a-zA-Z0-9_][a-zA-Z0-9_]*)/g,
+		replace_after: '$1'
+	},  // overline
+	'logic': {
+		pattern: /\~|\&|\|/
+	},
+	'arithmetic': {
+		pattern: /\+|-|\*|\//
+	},
+	'compare': {
+		pattern: /<=|>=|!=|=|<|>/
+	},
+	'assign': {
+		pattern: /\+|-|:=/
+	},
+	'channel': {
+		pattern: /!|\?/
+	},
+	'compose': {
+		pattern: /\|\||;|,/
+	},
+	'control': {
+		pattern: /\*\[|\[|\]|\||\[\]|->/
+	},
+	'comment': [
+		{
+			pattern: /(^|[^\\])\/\*[\s\S]*?\*\//,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true
+		}
+	],
+	'string': {
+		pattern: /(["'])(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+		greedy: true
+	},
+	'keyword': /\b(skip|null)\b/,
+	'boolean': /\b(true|false)\b/,
+	'function': /[a-z0-9_]+(?=\()/i,
+	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
+};
+
 
 Prism.languages.prs = {
 	'node_long': { 
@@ -662,6 +708,34 @@ Prism.languages.prs = {
 		pattern: /(["'])(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
 		greedy: true
 	},
+	'keyword': /\b(skip|null)\b/,
+	'boolean': /\b(true|false)\b/,
+	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
+};
+
+Prism.languages.rawprs = {
+	'logic': {
+		pattern: /\u00AC|\u2227|\u2228/
+	},
+	'arithmetic': {
+		pattern: /\+|-|\*|\//
+	},
+	'compare': {
+		pattern: /\u2264|\u2265|\u2260|=|<|>/
+	},
+	'assign': {
+		pattern: /\u21BE|\u21C2|:=/
+	},
+	'comment': [
+		{
+			pattern: /(^|[^\\])\/\*[\s\S]*?\*\//,
+			lookbehind: true
+		},
+		{
+			pattern: /(^|[^\\:])\/\/.*/,
+			lookbehind: true
+		}
+	],
 	'keyword': /\b(skip|null)\b/,
 	'boolean': /\b(true|false)\b/,
 	'number': /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
